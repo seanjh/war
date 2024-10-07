dd12ef7cf0cd27e6f420448cb946c6942cb35d47
Sean Herman <sherman1@rbi.com>
2024-10-06 23:45:36 -0400

    slug test

--- game/game_test.go
+++ game/game_test.go
@@ -35,3 +35,32 @@ func TestCardName(t *testing.T) {
 		})
 	}
 }
+
+func TestCardSlug(t *testing.T) {
+	testCases := []struct {
+		card     Card
+		expected string
+	}{
+		{
+			card:     Card{Suit: "C", Value: 2},
+			expected: "2C",
+		},
+		{
+			card:     Card{Suit: "H", Value: 3},
+			expected: "3H",
+		},
+		{
+			card:     Card{Suit: "D", Value: 11},
+			expected: "JD",
+		},
+		{
+			card:     Card{Suit: "S", Value: 14},
+			expected: "AS",
+		},
+	}
+	for _, c := range testCases {
+		t.Run("card slug", func(t *testing.T) {
+			assert.Equal(t, c.card.Slug(), c.expected)
+		})
+	}
+}
